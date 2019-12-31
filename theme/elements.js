4a4d169e6684c839e8ea94ac9b17a4854fd429f0
eremm <evanremmele@gmail.com>
2019-12-31 00:47:28 -0600

    Add more elements

--- theme/elements.js
+++ theme/elements.js
@@ -1,16 +1,16 @@
 const getCrunchyrollThemeElements = () => ([
   [ // Primary-colored elements
     ...document.getElementsByTagName('body'),
+    ...document.getElementsByClassName('main-tabs'),
   ],
   [ // Secondary-colored elements
     ...document.getElementsByClassName('site-header'),
     ...document.getElementsByClassName('template-container'),
     ...document.getElementsByClassName('old-template-body'),
+    ...[...document.getElementsByClassName('group-item')].flatMap((e) => [...e.getElementsByTagName('a')]),
+    // ...[...document.querySelectorAll('#tabs')].flatMap((e) => [...e.querySelectorAll('a:not(.selected):not(.sub-tabs-category)')]), //need to add custom css rule instead
   ],
   [ // Tertiary-colored elements
-    ...[...document.getElementsByClassName('group-item')].flatMap(
-      (item) => [...item.getElementsByTagName('a')]
-    ),
     ...document.getElementsByClassName('white-wrapper'),
     ...document.getElementsByClassName('header-searchbox'),
     ...document.getElementsByClassName('availability-notes-low'),
@@ -19,5 +19,9 @@ const getCrunchyrollThemeElements = () => ([
     ...document.getElementsByClassName('message-container'),
     ...document.getElementsByClassName('reply-form'),
     ...document.getElementsByClassName('show-all-simulcasts-button'),
+    ...document.getElementsByClassName('season-dropdown'),
+    document.getElementById('tabs'),
+    ...document.getElementsByClassName('sub-tabs'),
+    // ...[...document.querySelectorAll('#tabs')].flatMap((e) => [...e.querySelectorAll('a.selected:not(.sub-tabs-category)')]), //need to add custom css rule instead
   ],
-]);
+]).map((arr) => arr.filter((e) => !!e));
